ed7b111a4f32846ff720cc480ef6752b23e1ab6b
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-02-21 07:54:09 +0530

    lafad

---
+++ Delegatessc1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void Calculation(int a, int b);
+    public class Delegatessc1
+    {
+        public static void Addition(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+       public  static void Subtraction(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine(result);
+        }
+        public static void Multiplication(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine(result);
+        }
+        public static void Divison(int a, int b)
+        {
+            int result = a / b;
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Calculation obj = new Calculation(Delegatessc1.Addition);
+            obj.Invoke(10,31);
+            obj = Delegatessc1.Subtraction;
+            obj(300,31);
+            Console.ReadLine();
+        }
+    }
+}
